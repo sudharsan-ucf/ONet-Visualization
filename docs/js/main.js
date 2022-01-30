cbf698d6b8f3a476fcf39483bd389f178e2d76c2
Sudharsan Vaidhun <sudharsan.vaidhun@knights.ucf.edu>
2022-01-30 02:03:20 -0500

    Force initialized

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,18 +1,21 @@
 d3.json('data/hJson.json', function(data){
 
+  // Debugging Settings
   console.clear();
+  var jobs = ["Animal Trainers", "Bakers"];
+
+  // Plot configurations
+  var jobCircleRadius = 40,
+      skillCircleRadius = 20;
     
   var svgWidth, svgHeight,
-      jobCircleRadius = 40,
-      skillCircleRadius = 20,
       jobCircleParameters = [],
       skillCircleParameters = [],
       skills = [],
-      jobs = ["Animal Trainers", "Bakers"],
       jobValues = [],
-      jobNodes;
+      nodes = [], links = [];
 
-
+  // Populating the Job options
   for (var key in data) {
     if (data.hasOwnProperty(key)) {
       d3.selectAll(".job-select")
@@ -22,28 +25,34 @@ d3.json('data/hJson.json', function(data){
     }
   }
 
-  
-  d3.select('#canvas')
-  .classed("svg-container", true)
-  .style("padding-left", 0)
-  .style("padding-right", 0)
-  .append('svg')
-  .classed("svg-content-responsive", true)
-  .attr("id", "svg-canvas");
-  
+  // Creating the SVG canvas for drawing
+  var canvas = d3.select('#canvas')
+    .classed("svg-container", true)
+    .style("padding-left", 0)
+    .style("padding-right", 0)
+    .append('svg')
+    .classed("svg-content-responsive", true)
+    .attr("id", "svg-canvas");
   svgWidth = document.getElementById("canvas").getBoundingClientRect().width;
   svgHeight = document.getElementById("canvas").getBoundingClientRect().height;
-  
   d3.select("#svg-canvas")
-  .attr("width", svgWidth)
+    .attr("width", svgWidth)
     .attr("height", svgHeight);
     
+  
+  // Creating the nodes object
+  // Adding jobs to the nodes list
+  // Extracting the common skills
   for (var key in data) {
     if (data.hasOwnProperty(key)) {
       if (jobs.includes(key)){
         jobValues.push(data[key]["Code"]);
-        d3.select("svg").append("circle")
-        .classed("job-circle", true);
+        nodes.push({
+          name:key,
+          cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
+          cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
+          r:jobCircleRadius
+        });
         for (var skillIndex in data[key]["skill"]) {
           skills.push(data[key]["skill"][skillIndex]);
         }
@@ -51,189 +60,96 @@ d3.json('data/hJson.json', function(data){
     }
   }
   
+  // Simulating a chosen option
   document.getElementById("dropdown-firstjob").value = jobValues[0];
   document.getElementById("dropdown-secondjob").value = jobValues[1];
 
+  // Removing duplicates from skills
   skills = [...new Set(skills)];
-  
-  jobCircleParameters.push({
-    cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
-    cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
-    r:jobCircleRadius});
-  jobCircleParameters.push({
-    cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
-    cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
-    r:jobCircleRadius});
-      
-  for (let x = 0; x < skills.length; x++) {
-    d3.select("svg").append("circle")
-      .classed("skill-circle", true);
-    skillCircleParameters.push({
+
+  // Adding skills to the nodes list
+  for (var skill in skills) {
+    nodes.push({
+      name:skills[skill],
       cx:skillCircleRadius + Math.random()*(svgWidth -2*skillCircleRadius),
       cy:skillCircleRadius + Math.random()*(svgHeight -2*skillCircleRadius),
       r:skillCircleRadius
     });
   }
-      
-  d3.selectAll(".job-circle")
-    .data(jobCircleParameters)
-    .attr("cx", function(circleParameter, i){
-      return circleParameter.cx;
-    })
-    .attr("cy", function(circleParameter, i){
-      return circleParameter.cy;
-    })
-    .attr("r", function(circleParameter, i){
-      return circleParameter.r;
-    });
-    
-  d3.selectAll(".skill-circle")
-    .data(skillCircleParameters)
-    .attr("cx", function(circleParameter, i){
-      return circleParameter.cx;
-    })
-    .attr("cy", function(circleParameter, i){
-      return circleParameter.cy;
-    })
-    .attr("r", function(circleParameter, i){
-      return circleParameter.r;
-    });
+  for (var i=0; i<skills.length; i++){
+    links.push({
+      source:nodes[2+i], target:nodes[0] });
+    links.push({
+      source:nodes[2+i], target:nodes[1] });
+  }
 
+  // Creating the force layout
+  var force = d3.layout.force()
+                .gravity(0.1)
+                .charge(-2000)
+                .size([svgWidth,svgHeight]);
+  
+  var link =  canvas.selectAll('line')
+                    .data(links).enter().append('line')
+                    .attr('stroke', 'black')
+                    .attr('strokewidth', '4');
+  
+  // Creating a group element as nodes
+  var node =  canvas.selectAll('circle')  
+                    .data(nodes).enter() 
+                    .append('g') 
+                    .call(force.drag);
+  
+  node.append("circle")
+      .classed("job-circle", true)
+      .attr("cx", function(circleParameter, i){
+        return circleParameter.cx;
+      })
+      .attr("cy", function(circleParameter, i){
+        return circleParameter.cy;
+      })
+      .attr("r", function(circleParameter, i){
+        return circleParameter.r;
+      })
+      .attr('fill', function(circleParameter,i){
+        if ( i < 2 ) {
+          return '#f00';
+        } else {
+          return '#00f';
+        }})
+      .attr('strokewidth', function(circleParameter,i){
+        if ( i < 2 ) {
+          return '4';
+        } else {
+          return '1';
+        }})
+      .attr('stroke', function(circleParameter,i){
+        return 'black';
+      });
+  
+  node.append('text')
+      .text(function(d){ return d.name; })
+      .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
+      .attr('fill', function(d, i){ return 'black'; })
+      .attr('text-anchor', function(d, i) { return 'middle'; })
+      .attr('font-size', function(d, i){
+        if (i < 2 ) {
+              return '.6em';
+        } else {
+              return '.5em';    
+        }
+  })
+  
+  force.on('tick', function(e){
+    node.attr('transform', function(d, i){
+      return 'translate(' + d.cx + ','+ d.cy + ')'
+    })
+    link.attr('x1', function(d){ return d.source.cx; }) 
+        .attr('y1', function(d){ return d.source.cy; })
+        .attr('x2', function(d){ return d.target.cx; })
+        .attr('y2', function(d){ return d.target.cy; })
   });
-  
-  // console.log(data);
 
+  force.start();
 
-// //   var w = 800,
-// //       h =  800,
-// //       circleWidth = 1;
-  
-// //   var palette = {
-// //     "lightgray": "#000000",
-// //     "gray": "#708284",
-// //     "mediumgray": "#536870",
-// //     "blue": "#3B757F",
-// //   }
-
-// //   var colors = d3.scale.category10();
-// //   var second_node = 7
-// //   var nodes_1 = [
-// //     {name: "Desk Clerk"},
-// //     {name: "Critical Thinking", target: [0], value: 20 },
-// //     {name: "Reading Comprehension", target: [0], value: 20 },
-// //     {name: "Active Listening", target: [0,second_node], value: 20 },
-// //     {name: "Monitoring", target: [0,second_node], value: 20 },
-// //     {name: "Active Learning", target: [0], value: 20 },
-// //     {name: "Speaking", target: [0,second_node], value: 20 },
-// //     {name: "Waiter/Waitress"},
-// //   ]
-  
-// //   var second_node = nodes_1.length -1
-// //   var links = [];
-
-// //   for (var i = 0; i < nodes_1.length; i++){
-// //     if (nodes_1[i].target !== undefined) { 
-// //       for ( var x = 0; x < nodes_1[i].target.length; x++ ) 
-// //         links.push({
-// //           source: nodes_1[i],
-// //           target: nodes_1[nodes_1[i].target[x]]  
-// //         });
-// //     };
-// //   };
-
-// //   var myChart = d3.select('#main-window')
-// //     .append("div")
-// //       .classed("svg-container", true)
-// //     .append('svg')
-// //       // .attr("preserveAspectRatio", "xMinYMin meet")
-// //       .attr("viewBox", "0 0 1000 1000")
-// //       .classed("svg-content-responsive", true)
-
-//   var force = d3.layout.force()
-//     .nodes(nodes_1)
-//     .links([])
-//     .gravity(0.1)
-//     .charge(-2000)
-//     .size([w,h]); 
-
-//   var link = myChart.selectAll('line')
-//     .data(links).enter().append('line')
-//     .attr('stroke', palette.lightgray)
-//     .attr('strokewidth', '4');
-
-//   var node = myChart.selectAll('circle')  
-//     .data(nodes_1).enter()
-//     .append('g')
-//     .call(force.drag); 
-
-// node.append('circle')
-//     .attr('cx', function(d){return d.x; })
-//     .attr('cy', function(d){return d.y; })
-//     .attr('r', function(d,i){
-//       if ( i > 0 && i != second_node) {
-//         return circleWidth + d.value;
-//       } else {
-//         return circleWidth + 40;
-//       }
-//       // if ( i == 0 || i = second_node ) {
-//       //       return circleWidth + 40; 
-//       // } else {
-//       //       return circleWidth + d.value; 
-//       // }
-//     })
-//     .attr('fill', function(d,i){
-//       if ( i > 0 && i != second_node) {
-//         return colors(i);
-//       } else {
-//         return '#fff';
-//     }})
-//     .attr('strokewidth', function(d,i){
-//       if ( i > 0 && i != second_node) {
-//         return '1';
-//       } else {
-//         return '4';
-//     }})
-//     .attr('stroke', function(d,i){
-//       if ( i > 0 && i != second_node) {
-//         return 'black';
-//       } else {
-//         return 'black';
-//     }});
-
-// force.on('tick', function(e){
-//     node.attr('transform', function(d, i){
-//         return 'translate(' + d.x + ','+ d.y + ')'
-//         })
-//     link.attr('x1', function(d){ return d.source.x; }) 
-//         .attr('y1', function(d){ return d.source.y; })
-//         .attr('x2', function(d){ return d.target.x; })
-//         .attr('y2', function(d){ return d.target.y; })
-//         });
-
-// node.append('text')
-//     .text(function(d){
-//         return d.name;})
-//     .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
-//     .attr('fill', function(d, i){
-//         if ( i > 0 && d.value < 10 && i != second_node  ) {
-//             return palette.black;
-//         } else if ( i > 0 && d.value >10 && i != second_node ) {
-//             return palette.black;
-//         } else {
-//             return palette.black;
-//         }})
-//     .attr('text-anchor', function(d, i) {
-//         return 'middle';
-//         })
-//     .attr('font-size', function(d, i){
-//         if (i > 0 && i != second_node ) {
-//             return '.5em';
-//         } else {
-//             return '.6em';    
-//         }})
-
-
-
-// force.start();
-
-
+});
