3c65aef7924b660562d5e2bb71edaba73cdcb648
Sudharsan Vaidhun <sudharsan.vaidhun@knights.ucf.edu>
2022-02-10 18:14:52 -0500

    Store data in browser memory

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,154 +1,183 @@
+// Author: Sudharsan Vaidhun
+
+
+// Load the data into browser memory
 d3.json('data/hJson.json', function(data){
+  localStorage.setItem('data', JSON.stringify(data));
+});
 
-  // Populating the Job options
-  for (var key in data) {
-    if (data.hasOwnProperty(key)) {
-      d3.selectAll(".job-select")
-        .append("option")
-        .attr("value", data[key]["Code"])
-        .text(key);
-    }
+
+// Debugging Settings
+console.clear();
+
+
+// Local variables
+var data = JSON.parse(localStorage.getItem('data'));
+var colors = d3.scale.category10();
+var testJobs;
+
+
+// Populating the Job options
+for (var key in data) {
+  if (data.hasOwnProperty(key)) {
+    d3.selectAll(".job-select")
+      .append("option")
+      .attr("value", data[key]["Code"])
+      .text(key);
   }
+}
 
-  // Debugging Settings
-  console.clear();
-  var testJobs;
-  var colors = d3.scale.category10();
 
-  // Plot configurations
-  var jobCircleRadius = 40,
-      skillCircleRadius = 20;
-  var svgWidth, svgHeight,
-      jobCircleParameters = [],
-      skillCircleParameters = [],
-      skills = [],
-      jobValues = [],
-      nodes = [], links = [];
-      
-  // Creating the SVG canvas for drawing
-  var canvas = d3.select('#canvas')
-    .classed("svg-container", true)
-    .append('svg')
-    .classed("svg-content-responsive", true)
-    .attr("id", "svg-canvas");
-  svgWidth = document.getElementById("svg-canvas").getBoundingClientRect().width;
-  svgHeight = document.getElementById("svg-canvas").getBoundingClientRect().height;
-  // d3.select("#svg-canvas")
-  //   .attr("width", svgWidth)
-  //   .attr("height", svgHeight);
+// Plot configurations
+var jobCircleRadius = 40,
+    skillCircleRadius = 20;
+var svgWidth, svgHeight,
+    jobCircleParameters = [],
+    skillCircleParameters = [],
+    skills = [],
+    jobValues = [],
+    nodes = [], links = [];
     
+// Creating the SVG canvas for drawing
+var canvas = d3.select('#canvas')
+  .classed("svg-container", true)
+  .append('svg')
+  .classed("svg-content-responsive", true)
+  .attr("id", "svg-canvas");
+svgWidth = document.getElementById("svg-canvas").getBoundingClientRect().width;
+svgHeight = document.getElementById("svg-canvas").getBoundingClientRect().height;
+// d3.select("#svg-canvas")
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight);
   
-  // Creating the nodes object
-  // Adding jobs to the nodes list
-  // Extracting the common skills
 
-  var graphData = {
-    "nodes": [],
-    "edges": []
-  };
-  
-  testJobs = ["Animal Trainers", "Bakers"];
-  for (var key in data) {
-    if (data.hasOwnProperty(key)) {
-      if (testJobs.includes(key)){
-        graphData.nodes.push({
-          name: key,
-          cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
-          cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
-          r:jobCircleRadius
-        })
-        jobValues.push(data[key]["Code"]);
-        nodes.push({
-          name:key,
-          // cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
-          // cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
-          // r:jobCircleRadius
-        });
-        for (var skillIndex in data[key]["skill"]) {
-          skills.push(data[key]["skill"][skillIndex]);
-        }
+// Creating the nodes object
+// Adding jobs to the nodes list
+// Extracting the common skills
+
+var graphData = {
+  "nodes": [],
+  "edges": []
+};
+
+testJobs = ["Animal Trainers", "Bakers"];
+for (var key in data) {
+  if (data.hasOwnProperty(key)) {
+    if (testJobs.includes(key)){
+      graphData.nodes.push({
+        name: key,
+        cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
+        cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
+        r:jobCircleRadius
+      })
+      jobValues.push(data[key]["Code"]);
+      nodes.push({
+        name:key,
+        // cx:jobCircleRadius + Math.random()*(svgWidth -2*jobCircleRadius),
+        // cy:jobCircleRadius + Math.random()*(svgHeight -2*jobCircleRadius),
+        // r:jobCircleRadius
+      });
+      for (var skillIndex in data[key]["skill"]) {
+        skills.push(data[key]["skill"][skillIndex]);
       }
     }
   }
-  
-  // Simulating a chosen option
-  document.getElementById("dropdown-firstjob").value = jobValues[0];
-  document.getElementById("dropdown-secondjob").value = jobValues[1];
+}
 
-  // Removing duplicates from skills
-  skills = [...new Set(skills)];
+// Simulating a chosen option
+document.getElementById("dropdown-firstjob").value = jobValues[0];
+document.getElementById("dropdown-secondjob").value = jobValues[1];
 
-  // Adding skills to the nodes list
-  for (var skill in skills) {
-    nodes.push({
-      name:skills[skill],
-      // cx:skillCircleRadius + Math.random()*(svgWidth -2*skillCircleRadius),
-      // cy:skillCircleRadius + Math.random()*(svgHeight -2*skillCircleRadius),
-      // r:skillCircleRadius
-    });
-  }
-  for (var i=0; i<skills.length; i++){
-    links.push({
-      source:nodes[2+i], target:nodes[0] });
-    links.push({
-      source:nodes[2+i], target:nodes[1] });
-  }
+// Removing duplicates from skills
+skills = [...new Set(skills)];
 
-  // Creating the force layout
-  var force = d3.layout.force()
-                .nodes(nodes)
-                .links([])
-                .gravity(0.1)
-                // .linkDistance([50])
-                .charge([-200])
-                .size([svgWidth,svgHeight])
-                .start();
-  
-  var link =  canvas.selectAll('line')
-                    .data(links).enter()
-                    .append('line')
-                    .style('stroke', '#aaa')
-                    .style('stroke-width', '2');
-  
-  // Creating a group element as nodes
-  var node =  canvas.selectAll('circle')  
-                    .data(nodes).enter()
-                    .append('g') 
-                    .call(force.drag);
-  
-  node.append("circle")
-      .classed("job-circle", true)
-      .attr("cx", function(d){ return 0; return d.x; })
-      .attr("cy", function(d){ return 0; return d.y; })
-      .attr("r", function(d, i){ 
-        if ( i < 2 ) { return jobCircleRadius; }
-        else { return skillCircleRadius; }})
-      .attr('fill', function(d,i){
-        if ( i < 2 ) { return '#f00'; }
-        else { return colors(i); }})
-      .attr('strokewidth', function(d,i){
-        if ( i < 2 ) { return '4'; }
-        else { return '1'; }})
-      .attr('stroke', function(d,i){ return 'black'; });
-  
-  force.on("tick", function(e){
-    node.attr('transform', function(d, i){
-      return 'translate(' + d.x + ','+ d.y + ')'
-    })
-    link.attr('x1', function(d){ return d.source.x; }) 
-        .attr('y1', function(d){ return d.source.y; })
-        .attr('x2', function(d){ return d.target.x; })
-        .attr('y2', function(d){ return d.target.y; })
+// Adding skills to the nodes list
+for (var skill in skills) {
+  nodes.push({
+    name:skills[skill],
+    // cx:skillCircleRadius + Math.random()*(svgWidth -2*skillCircleRadius),
+    // cy:skillCircleRadius + Math.random()*(svgHeight -2*skillCircleRadius),
+    // r:skillCircleRadius
   });
+}
+for (var i=0; i<skills.length; i++){
+  links.push({
+    source:nodes[2+i], target:nodes[0] });
+  links.push({
+    source:nodes[2+i], target:nodes[1] });
+}
 
-  node.append('text')
-    .text(function(d){ return d.name; })
-    .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
-    .attr('fill', function(d, i){ return 'black'; })
-    .attr('text-anchor', function(d, i) { return 'middle'; })
-    .attr('font-size', function(d, i){
-      if (i < 2 ) { return '.6em'; } else { return '.5em'; }})
+// Creating the force layout
+var force = d3.layout.force()
+              .nodes(nodes)
+              .links([])
+              .gravity(0.1)
+              // .linkDistance([50])
+              .charge([-200])
+              .size([svgWidth,svgHeight])
+              .start();
 
-  // force.start();
+var link =  canvas.selectAll('line')
+                  .data(links).enter()
+                  .append('line')
+                  .style('stroke', '#eee')
+                  .style('stroke-width', '2');
 
+// Creating a group element as nodes
+var node =  canvas.selectAll('circle')  
+                  .data(nodes).enter()
+                  .append('g') 
+                  .call(force.drag);
+
+node.append("circle")
+    .classed("job-circle", true)
+    .attr("cx", function(d){ return 0; return d.x; })
+    .attr("cy", function(d){ return 0; return d.y; })
+    .attr("r", function(d, i){ 
+      if ( i < 2 ) { return jobCircleRadius; }
+      else { return skillCircleRadius; }})
+    .attr('fill', function(d,i){
+      if ( i < 2 ) { return '#f00'; }
+      else { return colors(i); }})
+    .attr('strokewidth', function(d,i){
+      if ( i < 2 ) { return '4'; }
+      else { return '1'; }})
+    .attr('stroke', function(d,i){ return 'black'; });
+
+force.on("tick", function(e){
+  node.attr('transform', function(d, i){
+    return 'translate(' + d.x + ','+ d.y + ')'
+  })
+  link.attr('x1', function(d){ return d.source.x; }) 
+      .attr('y1', function(d){ return d.source.y; })
+      .attr('x2', function(d){ return d.target.x; })
+      .attr('y2', function(d){ return d.target.y; })
 });
+
+node.append('text')
+  .text(function(d){ return d.name; })
+  .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
+  .attr('fill', function(d, i){ return 'black'; })
+  .attr('text-anchor', function(d, i) { return 'middle'; })
+  .attr('font-size', function(d, i){
+    if (i < 2 ) { return '.6em'; } else { return '.5em'; }})
+
+// force.start();
+
+
+
+
+function callFunction(){
+  var localCounter = JSON.parse(localStorage.getItem('counter'));
+  localCounter.count += 1;
+  console.log('Counter value is ' + localCounter.count);
+  localStorage.setItem('counter', JSON.stringify(localCounter));
+}
+
+function job1Changed(){
+  var localCounter = JSON.parse(localStorage.getItem('counter'));
+  localCounter.count += 1;
+  console.log('Counter value is ' + localCounter.count);
+  localStorage.setItem('counter', JSON.stringify(localCounter));
+}
+
